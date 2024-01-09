c9710593f851e12840f112cffa0908a0079482c0
phogatheena <109434462+phogatheena@users.noreply.github.com>
2024-01-09 15:56:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+for(let i =1;i<=20;i++)
+	{
+		if(i%3===0 && i%5===0)
+		{
+			alert("FizzBuzz"+"\n");
+        }
+       else if(i%3===0)
+	   {
+		   alert("Fizz"+"\n");
+	   }
+		else if(i%5===0)
+	   {
+		   alert("Buzz"+"\n");
+	   }
+		else
+		{
+			alert(i);
+		}
+		
+		
+	}
